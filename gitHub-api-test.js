540199538719cdf5f7a201638323d70186236a4e
Matthew Charles Chan <matthew.charles.chan@gmail.com>
2020-05-12 11:45:42 -0700

    remove un-needed dependencies

--- gitHub-api-test.js
+++ gitHub-api-test.js
@@ -1,13 +1,6 @@
 require('dotenv').config()
 
 const axios = require('axios')
-// const GitHub = require('github-api')
-// const gh = new GitHub({
-//   username: process.env.USERNAME,
-//   password: process.env.PASSWORD,
-//   token: process.env.TOKEN
-// })
-
 
 
 const URL = 'https://api.github.com'
