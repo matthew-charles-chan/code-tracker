630da8200daea2d3a69ebcd0ae01f7f3f712bc41
Matthew Charles Chan <matthew.charles.chan@gmail.com>
2020-05-13 18:48:08 -0700

    display date with Date.toLocaleString()

--- code-track/src/components/Commit.jsx
+++ code-track/src/components/Commit.jsx
@@ -1,12 +1,19 @@
 import React from "react";
 
 export default function Commit(props) {
-  console.log(props.commit.date);
   return (
     <li>
       <h3>{props.commit.repo.split("/")[1]}</h3>
       <ul>
-        {/* <li>Date: {props.commit.date}</li> */}
+        <li>
+          Date:{" "}
+          {props.commit.date.toLocaleString("en-US", {
+            weekday: "long",
+            year: "numeric",
+            month: "long",
+            day: "numeric",
+          })}
+        </li>
         <li>Message: {props.commit.message}</li>
       </ul>
     </li>
